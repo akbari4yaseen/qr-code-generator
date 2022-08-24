ed6288c55ccbc6d622107c35c39897ba5896f2e3
Yaseen Akbari <akbari_yaseen@yahoo.com>
2022-08-24 21:47:04 +0430

    added files

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# qr-code-generator
-QR Code Generator generates QR Code from inserted URL. Then you can export and save the generated QR Code
+# QR Code Generator
+
+> QR Code Generator generates QR Code from inserted URL. Then you can export and save the generated
+
+Screenshoot
+
+![Screenshoot](app_screenshot.jpg?raw=true)
+
+[Live Demo]()
+
+## Built With
+
+- HTML
+- Tailwind CSS
+- JavaScript
+- QR Code JS
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+### `git clone https://github.com/akbari4yaseen/qr-code-generator.git`
+
+### Run tests
+
+In the project directory, you can open index.html
+
+## Author
+
+👤 **Yaseen Akbari**
+
+- GitHub: [@githubhandle](https://github.com/akbari4yaseen)
+- Twitter: [@twitterhandle](https://twitter.com/AkbariYaseen)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/yaseen-akbari)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Thank you Microverse for this big opportunity
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
---
+++ js/script.js
@@ -0,0 +1,60 @@
+// Tailwind Configration
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
+
+const generateForm = document.querySelector("#qrcode-form");
+const spinner = document.querySelector("#spinner");
+const qr = document.querySelector("#qrcode");
+const url = document.querySelector("#url");
+const size = document.querySelector("#size");
+
+const onGenerateSubmit = (e) => {
+  e.preventDefault();
+  clearUI();
+  displaySpinner("block");
+  setTimeout(() => {
+    displaySpinner("none");
+    generateQRcode(url.value, size.value);
+
+    setTimeout(() => {
+      const saveUrl = qr.querySelector("img").src;
+      createSaveBtn(saveUrl);
+    }, 50);
+  }, 1000);
+};
+
+const displaySpinner = (s) => {
+  spinner.style.display = s;
+};
+generateForm.addEventListener("submit", onGenerateSubmit);
+
+const generateQRcode = (url, size) => {
+  const qrcode = new QRCode(qr, {
+    text: url,
+    width: size,
+    height: size,
+  });
+};
+
+const clearUI = () => {
+  qr.innerHTML = "";
+  document.querySelector("#save-button")?.remove();
+};
+
+const createSaveBtn = (saveUrl) => {
+  const saveButton = document.createElement("a");
+  saveButton.id = "save-button";
+  saveButton.classList =
+    "bg-blue-900 text-white font-bold py-2 my-5 rounded w-1/3 m-auto hover:bg-blue-700";
+  saveButton.href = saveUrl;
+  saveButton.download = "qrcode";
+  saveButton.innerHTML = "Export Image";
+  document.querySelector("#generated").appendChild(saveButton);
+};
